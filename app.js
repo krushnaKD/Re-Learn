5d7ae10f27c1bfa086caa4d4af5ebc39b224c24d
ankadekrushna01@gmail.com <ankadekrushna01@gmail.com>
2025-11-26 22:00:49 +0530

    kay pan

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/home',(req,res)=>{
     res.send('hee ! Welcome back Hommie!')
 })
 
+app.post()
+app.get('/new',(req,res)=>{
+    res.send("kay sangu ranii mala gav sutana kas sangu ranii mala gav sutana")
+})
+
 const Port = 5000
 
 app.listen(Port,()=>{
