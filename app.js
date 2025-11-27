ef08a02a8d7835c7b6e57985a63855d7763a570d
ankadekrushna01@gmail.com <ankadekrushna01@gmail.com>
2025-11-27 22:10:15 +0530

    created post methods and read some documentation

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ app.get('/home',(req,res)=>{
     res.send('hee ! Welcome back Hommie!')
 })
 
-app.post()
+app.post('/signup',(req,res)=>{
+    const {name,age,place} = req.body;
+    let arr = {
+        name,
+        age,
+        place
+    }
+    res.json({
+        msg:"signup has been done",
+        arr
+    })
+})
 app.get('/new',(req,res)=>{
     res.send("kay sangu ranii mala gav sutana kas sangu ranii mala gav sutana")
 })
